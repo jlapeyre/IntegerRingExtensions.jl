4d7a194052001eec0dd0cda0f08fafb1eba27380
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-08-11 21:44:04 -0400

    check

--- test/zrot_tests.jl
+++ test/zrot_tests.jl
@@ -1,19 +1,13 @@
 @testset "ZRot" begin
     for theta in (1/2, 1/4, 0.5678, Dar(1/4), Dar(1//4))
-
         rz = zrot(theta)
         @test isapprox(theta, get_theta(rz))
         ang = - 2 * angle(rz[1])
         @test isapprox(ang, get_theta(rz))
-
-        # rz2 = zrotpi(theta/pi)
-        # @test isapprox(theta, get_theta(rz2))
-        # ang = - 2 * angle(rz2[1])
-        # @test isapprox(ang, get_theta(rz2))
-
-        # rz3 = zrothalfpi(theta/(2*pi))
-        # @test isapprox(theta, get_theta(rz3))
-        # ang = - 2 * angle(rz2[1])
-        # @test isapprox(ang, get_theta(rz3))
     end
+
+    rz1 = zrot(Dar(1//8))
+    rz2 = zrot(Dar(3//8))
+    rz3 = rz1 * rz2
+    @test get_theta(rz3) === Dar(1//2)
 end
