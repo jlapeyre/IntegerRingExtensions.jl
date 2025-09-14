b215f765ef113d380c20a1c3fd4fff021ce8199a
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-09-14 15:44:08 -0400

    check

--- src/matrices.jl
+++ src/matrices.jl
@@ -187,6 +187,8 @@ function Base.show(io::IO, m::Matrix2x2)
 end
 
 function Base.show(io::IO, ::PRETTY, v::AbstractVector2)
+    summary(io, v)
+    println(io)
     (v1, v2) = elements(v)
     show(io, PRETTY(), v1)
     println(io)
@@ -1066,65 +1068,5 @@ function Base.:^(sm::ScaleMatrix2x2, n::Integer)
 end
 
 det(sm::ScaleMatrix2x2) = (sm.s * sm.s) * det(sm.m)
-
-const I2x2 = Matrix2x2(1, 0, 0, 1)
-const X = Matrix2x2(0, 1, 1, 0)
-const Y = Matrix2x2(complex(0), 1im, -1im, complex(0))
-const Z = Matrix2x2(1, 0, 0, -1)
-
-random_density_matrix2x2(;pure=false) = random_density_matrix2x2(Float64;pure=pure)
-function random_density_matrix2x2(::Type{T} ;pure=false) where {T <: AbstractFloat}
-    (px, py, pz) = rand(T, 3)
-    s = sqrt(px*px + py*py + pz*pz)
-    pure || (s = s * rand(T))
-    (px, py, pz) = (px, py, pz) ./ s
-    return 0.5 * (I2x2 + px * X + py * Y + pz * Z)
-end
-
-struct DensityMatrix2x2{T<:AbstractFloat} <: AbstractNormal2x2{Complex{T}}
-    px::T
-    py::T
-    pz::T
-end
-
-polarization(rho::DensityMatrix2x2) = (rho.px, rho.py, rho.pz)
-
-function DensityMatrix2x2(m::AbstractMatrix2x2)
-   DensityMatrix2x2(real(tr(X * m)), real(tr(Y * m)), real(tr(Z * m)))
-end
-
-@inline _m00(r::DensityMatrix2x2) = (1+r.pz)/2
-@inline _m01(r::DensityMatrix2x2) = (r.px - r.py * im)/2
-@inline _m10(r::DensityMatrix2x2) = (r.px + r.py * im)/2
-@inline _m11(r::DensityMatrix2x2) = (1-r.pz)/2
-
-@inline function Matrix2x2(rho::DensityMatrix2x2{T}) where {T <: AbstractFloat}
-    Matrix2x2{Complex{T}}(rho)
-    # (px, py, pz) = polarization(rho)
-    # Matrix2x2(_m00(rho), _m10(rho), _m01(rho), _m11(rho))
-end
-
-@inline function Matrix2x2{Complex{T}}(rho::DensityMatrix2x2) where {T<:AbstractFloat}
-    (px, py, pz) = polarization(rho)
-    CT = Complex{T}
-    Matrix2x2(CT(_m00(rho)), CT(_m10(rho)), CT(_m01(rho)), CT(_m11(rho)))
-end
-
-function Base.getindex(rho::DensityMatrix2x2, i::Integer)
-    i == 1 && return complex(_m00(rho))
-    i == 2 && return _m10(rho)
-    i == 3 && return _m01(rho)
-    i == 4 && return complex(_m11(rho))
-    throw(BoundsError(rho, i))
-end
-
-tr(rho::DensityMatrix2x2) = 1
-LinearAlgebra.ishermitian(rho::DensityMatrix2x2) = true
-diag(rho::DensityMatrix2x2) = Vector2(real(1+rho.pz)/2, real(1-rho.pz)/2)
-
-function det(rho::DensityMatrix2x2)
-    (rx, ry, rz) = polarization(rho)
-    1 - rx*rx- ry*ry- rz*rz
-end
 
 end # module Matrices2x2
---
+++ src/qm_matrices.jl
@@ -0,0 +1,102 @@
+module QMMatrices
+
+import LinearAlgebra
+import LinearAlgebra: eigvals, svdvals, opnorm, tr, det, diag, diagm, eigvecs, norm, normalize
+import IsApprox:  AbstractApprox, Equal, Approx # isunitary, isinvolution
+
+import ..Matrices2x2: AbstractMatrix2x2, Matrix2x2, AbstractNormal2x2
+
+const I2x2 = Matrix2x2(1, 0, 0, 1)
+const X = Matrix2x2(0, 1, 1, 0)
+const Y = Matrix2x2(complex(0), 1im, -1im, complex(0))
+const Z = Matrix2x2(1, 0, 0, -1)
+
+random_density_matrix2x2(;pure=false) = random_density_matrix2x2(Float64;pure=pure)
+function random_density_matrix2x2(::Type{T} ;pure=false) where {T <: AbstractFloat}
+    (px, py, pz) = rand(T, 3)
+    s = sqrt(px*px + py*py + pz*pz)
+    pure || (s = s * rand(T))
+    (px, py, pz) = (px, py, pz) ./ s
+    return 0.5 * (I2x2 + px * X + py * Y + pz * Z)
+end
+
+struct DensityMatrix2x2{T<:AbstractFloat} <: AbstractNormal2x2{Complex{T}}
+    px::T
+    py::T
+    pz::T
+end
+
+# Need a sampler, and interface, etc.
+# function Random.rand(rng::Random.AbstractRNG, ::Random.SamplerType{DensityMatrix2x2})
+#     DensityMatrix2x2(random_density_matrix2x2(rng))
+# end
+
+"""
+    polarization(rho::DensityMatrix2x2)
+
+Return the polarization vector of `rho`.
+"""
+polarization(rho::DensityMatrix2x2) = (rho.px, rho.py, rho.pz)
+
+"""
+    polarization_norm(rho::DensityMatrix2x2)
+
+Return the square of the norm of the polarization vector of `rho`.
+"""
+function polarization_norm(rho::DensityMatrix2x2)
+    (x, y, z) = polarization(rho)
+    x*x + y*y + z*z
+end
+
+function DensityMatrix2x2(m::AbstractMatrix2x2)
+   DensityMatrix2x2(real(tr(X * m)), real(tr(Y * m)), real(tr(Z * m)))
+end
+
+@inline _m00(r::DensityMatrix2x2) = (1+r.pz)/2
+@inline _m01(r::DensityMatrix2x2) = (r.px - r.py * im)/2
+@inline _m10(r::DensityMatrix2x2) = (r.px + r.py * im)/2
+@inline _m11(r::DensityMatrix2x2) = (1-r.pz)/2
+
+@inline function Matrix2x2(rho::DensityMatrix2x2{T}) where {T <: AbstractFloat}
+    Matrix2x2{Complex{T}}(rho)
+    # (px, py, pz) = polarization(rho)
+    # Matrix2x2(_m00(rho), _m10(rho), _m01(rho), _m11(rho))
+end
+
+@inline function Matrix2x2{Complex{T}}(rho::DensityMatrix2x2) where {T<:AbstractFloat}
+    (px, py, pz) = polarization(rho)
+    CT = Complex{T}
+    Matrix2x2(CT(_m00(rho)), CT(_m10(rho)), CT(_m01(rho)), CT(_m11(rho)))
+end
+
+function Base.getindex(rho::DensityMatrix2x2, i::Integer)
+    i == 1 && return complex(_m00(rho))
+    i == 2 && return _m10(rho)
+    i == 3 && return _m01(rho)
+    i == 4 && return complex(_m11(rho))
+    throw(BoundsError(rho, i))
+end
+
+tr(rho::DensityMatrix2x2) = 1
+
+# All unitary matrices are normal. All Hermitian matrices are normal
+# Not all Hermitian matrices are unitary. So matrices are not categorized
+# into a tree by these features.
+# So it would be useful to implement traits for these features.
+LinearAlgebra.ishermitian(rho::DensityMatrix2x2) = true
+LinearAlgebra.ishermitian(rho::DensityMatrix2x2, ::AbstractApprox) = true
+LinearAlgebra.ishermitian(rho::DensityMatrix2x2, ::Approx) = true
+Base.adjoint(rho::DensityMatrix2x2) = rho
+
+diag(rho::DensityMatrix2x2) = Vector2(real(1+rho.pz)/2, real(1-rho.pz)/2)
+
+ispure(rho::DensityMatrix2x2) = ispure(rho, Equal())
+function ispure(rho::DensityMatrix2x2, approx::AbstractApprox)
+    isapprox(1 + polarization_norm(rho), 2, approx)
+end
+
+function det(rho::DensityMatrix2x2)
+    1 - polarization_norm(rho)
+end
+
+end # module QMMatrices
