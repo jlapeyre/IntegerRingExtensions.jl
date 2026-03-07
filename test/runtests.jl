1e533ce597e2db75255f99815863eb9d94a239c9
John Lapeyre <1969884+jlapeyre@users.noreply.github.com>
2026-03-07 00:29:26 -0500

    check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using IntegerExtensions
 using Test
 
+include("test_aqua.jl")
+
 push!(LOAD_PATH, joinpath(@__DIR__, "..", "lib"))
 
 # Angles2
@@ -9,8 +11,10 @@ include(joinpath(@__DIR__, "..", "lib", "Angles2", "test", "runtests.jl"))
 # Functionsecants
 include(joinpath(@__DIR__, "..", "lib", "FunctionSecants", "test", "runtests.jl"))
 
+# RootOnes
+include(joinpath(@__DIR__, "..", "lib", "RootOnes", "test", "runtests.jl"))
+
 include("matrices_tests.jl")
-include("rootone_tests.jl")
 include("singleton_conversion_tests.jl")
 include("composition_tests.jl")
 include("quadratic_ring_tests.jl")
@@ -19,4 +23,3 @@ include("dyadic_tests.jl")
 include("common_tests.jl")
 include("zrot_tests.jl")
 include("ringmatrices_tests.jl")
-# include("test_aqua.jl")
